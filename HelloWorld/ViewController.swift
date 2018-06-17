d5c01789875b8ca9ec29f6f99a5077bf5e728a3a
Paitoon <toon251hw@gmail.com>
2018-06-17 08:50:56 +0700

    first commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Patioon Burapavijitnon on 6/17/18.
+//  Copyright © 2018 Patioon Burapavijitnon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
